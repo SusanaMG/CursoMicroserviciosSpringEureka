d6009eeb17d5c86bd8dae93a26c8ede0795347e4
susanamg <susana.marquez@atos.net>
2021-01-25 12:54:45 +0100

    Creado el microservicio productos

---
+++ springboot-servicio-productos/src/main/java/com/formacionbdi/springboot/app/productos/models/service/IProductoService.java
@@ -0,0 +1,17 @@
+package com.formacionbdi.springboot.app.productos.models.service;
+
+import java.util.List;
+
+import com.formacionbdi.springboot.app.productos.models.entity.Producto;
+
+public interface IProductoService {
+	
+	// Métodos que implementa la interfaz. 
+	//El nomnbre del metodo no importa pero ponemos el mismo que está en el repositorio CrudRepository.class
+	// referenciado en ProductoDao.java
+	
+	public List<Producto> findAll();
+	
+	public Producto findById(Long id);
+	
+}
