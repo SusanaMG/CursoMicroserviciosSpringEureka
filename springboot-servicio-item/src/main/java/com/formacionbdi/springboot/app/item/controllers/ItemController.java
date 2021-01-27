ab5e959ade49c22df5e92c31d9a1514ad77e2e29
susanamg <susana.marquez@atos.net>
2021-01-27 21:34:42 +0100

    Tema 4. Configurando profiles en servicio items. Con metodos

--- springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
+++ springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
@@ -1,9 +1,18 @@
 package com.formacionbdi.springboot.app.item.controllers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,11 +23,20 @@ import com.formacionbdi.springboot.app.item.models.service.ItemService;
 @RestController
 public class ItemController {
 	
+	private Logger log = LoggerFactory.getLogger(ItemController.class);
+	
+	@Autowired
+	private Environment env;
+	
 	@Autowired
 	@Qualifier("serviceFeign")
 	//@Qualifier("serviceRestTemplate")		//En el caso de usar RestTemplate
 	private ItemService itemService;
 	
+	@Value("${configuracion.texto}")
+	private String texto;
+	
+
 	// Métodos handler del controlador
 	
 	@GetMapping("/listar")
@@ -30,5 +48,25 @@ public class ItemController {
 	public Item detalle(@PathVariable Long id, @PathVariable Integer cantidad) {
 	return itemService.findById(id, cantidad);
 	}
+	
+	@GetMapping("/obtener-config")
+	public ResponseEntity<?> obtenerConfig(@Value("${server.port}") String puerto) {
+
+		log.info(texto);
+
+		// Dos formas de usar las propiedades de application.properties:
+		// 1. Declarandola con @Value como variable.
+		// 2. Pasándosela a la función como parámetro con @Value
+		Map<String, String> json = new HashMap<>();
+		json.put("texto", texto);
+		json.put("puerto", puerto);
+		
+		if(env.getActiveProfiles().length>0 && env.getActiveProfiles()[0].equals("dev")) {
+			json.put("autor.nombre", env.getProperty("configuracion.autor.nombre"));
+			json.put("autor.email", env.getProperty("configuracion.autor.email"));
+		}
+		
+		return new ResponseEntity<Map<String, String>>(json, HttpStatus.OK);
+	}	
 	
 }
--- springboot-servicio-item2/src/main/java/com/formacionbdi/springboot/app/item2/controllers/ItemController.java
+++ springboot-servicio-item2/src/main/java/com/formacionbdi/springboot/app/item2/controllers/ItemController.java
@@ -1,9 +1,17 @@
 package com.formacionbdi.springboot.app.item2.controllers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,10 +24,16 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 @RestController
 public class ItemController {
 	
+	private Logger log = LoggerFactory.getLogger(ItemController.class);
+	
 	@Autowired
 	@Qualifier("serviceFeign")
 	//@Qualifier("serviceRestTemplate")		//En el caso de usar RestTemplate
 	private ItemService itemService;
+	
+	@Value("${configuracion.texto}")
+	private String texto;
+	
 	
 	// Métodos handler del controlador
 	
@@ -28,13 +42,12 @@ public class ItemController {
 		return itemService.findAll();
 	}
 	
-	//Método alternativo para cuando haa errores
+	//Método alternativo para cuando hay errores 
 	@HystrixCommand(fallbackMethod = "metodoAlternativo") 
 	@GetMapping("/ver/{id}/cantidad/{cantidad}")
 	public Item detalle(@PathVariable Long id, @PathVariable Integer cantidad) {
 		return itemService.findById(id, cantidad);
 	}
-	
 	
 	//Retorna un objeto Item alternativo en caso de error
 	public Item metodoAlternativo(Long id, Integer cantidad) {
@@ -48,4 +61,20 @@ public class ItemController {
 		item.setProducto(producto);		
 		return item;
 	}
+	
+	@GetMapping("/obtener-config")
+	public ResponseEntity<?> obtenerConfig(@Value("${server.port}") String puerto) {
+
+		log.info(texto);
+
+		// Dos formas de usar las propiedades de application.properties:
+		// 1. Declarandola con @Value como variable.
+		// 2. Pasándosela a la función como parámetro con @Value
+		Map<String, String> json = new HashMap<>();
+		json.put("texto", texto);
+		json.put("puerto", puerto);
+
+		return new ResponseEntity<Map<String, String>>(json, HttpStatus.OK);
+	}
+	
 }
