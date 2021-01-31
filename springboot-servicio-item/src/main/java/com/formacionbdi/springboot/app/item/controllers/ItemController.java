ed3bad3e4c24ec4449fdd6f2363713a0a895f726
susanamg <susana.marquez@atos.net>
2021-01-31 21:43:33 +0100

    Actualizacion

--- springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
+++ springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
@@ -1,8 +1,8 @@
 package com.formacionbdi.springboot.app.item.controllers;
 
-//import java.util.HashMap;
+import java.util.HashMap;
 import java.util.List;
-//import java.util.Map;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,15 +10,19 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.core.env.Environment;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.formacionbdi.springboot.app.item.models.Item;
+import com.formacionbdi.springboot.app.item.models.Producto;
 import com.formacionbdi.springboot.app.item.models.service.ItemService;
 
 @RefreshScope
@@ -75,5 +79,36 @@ public class ItemController {
 		return new ResponseEntity<Map<String, String>>(json, HttpStatus.OK);
 	}	
 	*/
+	
+	@PostMapping("/crear")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Producto crear(@RequestBody Producto producto) {
+		return itemService.save(producto);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 }
