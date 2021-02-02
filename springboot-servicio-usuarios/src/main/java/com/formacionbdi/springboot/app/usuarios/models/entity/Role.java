8ef32171a3905358512d4e274f9b48bad3dc2bb0
susanamg <susana.marquez@atos.net>
2021-02-02 13:45:46 +0100

    Tema 7. Creando la relacion de muchos a muchos entre Usuario y Role

--- springboot-servicio-usuarios/src/main/java/com/formacionbdi/springboot/app/usuarios/models/entity/Role.java
+++ springboot-servicio-usuarios/src/main/java/com/formacionbdi/springboot/app/usuarios/models/entity/Role.java
@@ -9,6 +9,10 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+//import java.util.List;
+//import javax.persistence.FetchType;
+//import javax.persistence.ManyToMany;
+
 @Entity
 @Table(name = "roles")
 public class Role implements Serializable {
@@ -22,6 +26,12 @@ public class Role implements Serializable {
 	@Column(unique = true, length = 30)
 	private String nombre;
 
+	//Si se necesitase que fuera bidireccional la relación ManyToMany también se pondría este código en esta clase
+	/*
+	@ManyToMany(fetch = FetchType.LAZY, mappedBy = "roles")
+	private List<Usuario> usuarios;
+	*/
+	
 	// GETTERS AND SETTERS
 
 	public Long getId() {
@@ -39,5 +49,16 @@ public class Role implements Serializable {
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+	
+	//Si se necesitase que fuera bidireccional la relación ManyToMany también se pondría este código en esta clase
+	/*
+	public List<Usuario> getUsuarios() {
+		return usuarios;
+	}
+
+	public void setUsuarios(List<Usuario> usuarios) {
+		this.usuarios = usuarios;
+	}
+	*/
 
 }
--- springboot-servicio-usuarios/src/main/java/com/formacionbdi/springboot/app/usuarios/models/entity/Usuario.java
+++ springboot-servicio-usuarios/src/main/java/com/formacionbdi/springboot/app/usuarios/models/entity/Usuario.java
@@ -1,13 +1,19 @@
 package com.formacionbdi.springboot.app.usuarios.models.entity;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 @Entity
 @Table(name = "usuarios")
@@ -32,8 +38,17 @@ public class Usuario implements Serializable {
 	@Column(unique = true, length = 100)
 	private String email;
 
-	//GETTERS AND SETTERS
+	@ManyToMany(fetch = FetchType.LAZY)
+	//Customizar la tabla intermedia: nombre tabla, FK de la principal (dueña de la relación), 
+	//FK de la otra parte de la relación, 
+	//restricción para que un usuario no pueda tener el rol repetido
+	@JoinTable(name = "usuarios_roles", joinColumns = @JoinColumn(name = "usuario_id"), 
+		inverseJoinColumns = @JoinColumn(name = "role_id"), 
+		uniqueConstraints = {@UniqueConstraint(columnNames = {"usuario_id", "role_id"})})
+	private List<Role> roles;
 	
+	//GETTERS AND SETTERS
+
 	public Long getId() {
 		return id;
 	}
@@ -89,5 +104,12 @@ public class Usuario implements Serializable {
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	
+	public List<Role> getRoles() {
+		return roles;
+	}
 
+	public void setRoles(List<Role> roles) {
+		this.roles = roles;
+	}
 }
