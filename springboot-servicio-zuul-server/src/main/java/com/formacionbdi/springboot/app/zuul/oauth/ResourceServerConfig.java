951d1429c0faa6cdc676ac245f514f8a57e5c97c
susanamg <susana.marquez@atos.net>
2021-02-08 21:12:25 +0100

    Tema 7. Creando configuracion de OAuth en el Servidor de Configuracion

--- springboot-servicio-zuul-server/src/main/java/com/formacionbdi/springboot/app/zuul/oauth/ResourceServerConfig.java
+++ springboot-servicio-zuul-server/src/main/java/com/formacionbdi/springboot/app/zuul/oauth/ResourceServerConfig.java
@@ -1,5 +1,7 @@
 package com.formacionbdi.springboot.app.zuul.oauth;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -10,10 +12,14 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Res
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
+@RefreshScope
 @Configuration
 @EnableResourceServer				//Habilitar la configuración del servidor de recurso
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 	 
+	@Value("${config.security.oauth.jwt.key}")
+	private String jwtKey;
+	
 	//Método para configurar el token
 	@Override
 	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
@@ -64,7 +70,9 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 	public JwtAccessTokenConverter accessTokenConverter() {
 
 		JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
-		tokenConverter.setSigningKey("algun_codigo_secreto_aeiou");  //El mismo código secreto o llave
+		//Lo tenemos configurado en bootstarp.properties
+		//tokenConverter.setSigningKey("algun_codigo_secreto_aeiou");  //El mismo código secreto o llave
+		tokenConverter.setSigningKey(jwtKey);  //El mismo código secreto o llave
 
 		return tokenConverter;
 	}
